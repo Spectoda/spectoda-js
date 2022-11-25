226a380c90811be30460c55db0236d33be38377d
Michal Kucera <michal.kucera04@gmail.com>
2022-11-25 18:32:58 +0100

    centralize unknown error

--- errors/errorLibrary.ts
+++ errors/errorLibrary.ts
@@ -48,6 +48,20 @@ export const general = {
     title: "Device Disconnected",
     message: "The device has been disconnected. Please reconnect the device and try again.",
   },
+  // Code: {
+  //   title: "",
+  //   message: "",
+  // },
+  Code: {
+    title: "",
+    message: "",
+  },
 };
+
+// Appears when error is not defined above
+export const unknownError = (errorCode: string) => ({
+  title: "Unknown Error",
+  message: "An unknown error has occurred. Please contact us for support. Error code: " + errorCode,
+});
 
 export type code = keyof typeof general & keyof typeof nara & keyof typeof studio;
