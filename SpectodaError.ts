4af47778acf36d014f6d4173e31cc45f3c125a2b
Michal Kučera <michal.kucera04@gmail.com>
2023-05-09 18:18:15 +0200

    cleanup

--- SpectodaError.ts
+++ SpectodaError.ts
@@ -1,5 +1,4 @@
-import { ErrorCode } from "./errors/errorLibrary";
-import { ErrorFormat, unknownError } from "./errors/errorLibrary";
+import { ErrorFormat, ErrorCode, unknownError } from "./errors/errorLibrary";
 import { general, app, studio } from "./errors/errorLibrary";
 
 export class SpectodaError extends Error {
@@ -15,10 +14,17 @@ export class SpectodaError extends Error {
   }
 }
 
-type env = "studio" | "nara";
+type env = "app" | "studio";
+// Using @ts-ignore as we can guarantee the the error code is always found
 export const getError = (errorCode: ErrorCode, env?: env): ErrorFormat => {
-  if (env === "nara" && errorCode in app) return app[errorCode]!;
-  if (env === "studio" && errorCode in studio) return studio[errorCode]!;
-  if (errorCode in general) return general[errorCode]!;
-  else return unknownError(errorCode);
+  // @ts-ignore
+  if (env === "app" && errorCode in app) return app[errorCode] || unknownError;
+  // @ts-ignore
+  if (env === "studio" && errorCode in studio) return studio[errorCode] || unknownError;
+  if (errorCode in general) return general[errorCode] || unknownError;
+  else return unknownError;
+};
+
+export const throwError = (errorCode: ErrorCode) => {
+  throw new Error(errorCode);
 };
