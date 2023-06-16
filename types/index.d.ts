1d82745b387796bbce38af0d9a1e457a6cc90698
Michal Kučera <michal.kucera04@gmail.com>
2023-06-16 18:12:10 +0200

    Fix typescript declaration module

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,10 @@
 declare module "@spectoda/spectoda-js" {
-  // todo typescriptize spectoda-js
+  export function createNanoEvents(): any;
+  export function setLoggingLevel(...props: any[]): any;
+  export class Spectoda {
+    constructor(...props: any[]);
+    assignConnector(...props: any[]): any;
+    assignOwnerKey(...props: any[]): any;
+    assignOwnerSignature(...props: any[]): any;
+  }
 }
