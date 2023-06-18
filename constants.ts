3f3089892fa59c5e4cc054ee8ed0aa4ba5bab19a
Matěj Suchánek <jsem@matejsuchanek.cz>
2023-06-18 20:13:32 +0200

    Move constants from 0.9-wasm

---
+++ constants.ts
@@ -0,0 +1,14 @@
+export const VALUE_TYPE = {
+  TIMESTAMP: 32,
+  LABEL: 31,
+  PERCENTAGE: 30,
+  NUMBER: 29,
+  VALUE_ARRAY: 27,
+  COLOR: 26,
+  TRIPLE: 25,
+  PIXELS: 19,
+  VALUE_ADDRESS: 18,
+  BOOL: 2,
+  NULL: 1,
+  UNDEFINED: 0,
+} as const;
