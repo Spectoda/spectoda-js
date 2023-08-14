dff605833577da0d9faa7eac34de19c4088f1f8b
Matěj Suchánek <jsem@matejsuchanek.cz>
2023-08-14 13:46:21 +0200

    feat: add scream commits into slack and clickup

---
+++ scripts/slack-commit-share.js
@@ -0,0 +1,116 @@
+const axios = require("axios");
+const { execSync } = require("child_process");
+
+const TARGET_CHANNEL_ID = "C05GTKQ5JHW"; // #monorepo-commits
+
+const slackApi = axios.create({
+  baseURL: "https://slack.com/api",
+  headers: {
+    Authorization: `Bearer xoxb-1228490764037-5541178943398-x7t5S3Y0DUPYclCH99FSsIYi`,
+    "Content-Type": "application/json",
+  },
+});
+
+async function postCommitToSlack() {
+  try {
+    // Get the most recent commit message, author, commit hash and branch name
+    const commitMessage = execSync("git log -1 --pretty=%B", {
+      encoding: "utf8",
+    });
+    const commitAuthor = execSync('git log -1 --pretty=format:"%an"', {
+      encoding: "utf8",
+    });
+    const commitHash = execSync('git log -1 --pretty=format:"%H"', {
+      encoding: "utf8",
+    });
+
+    let branchName = execSync("git branch --show-current", {
+      encoding: "utf8",
+    }).trim();
+
+    // Handle detached head
+    if (!branchName) {
+      branchName = execSync("git name-rev --name-only HEAD", {
+        encoding: "utf8",
+      }).trim();
+    }
+
+    let tagName;
+    try {
+      tagName = execSync("git describe --tags --exact-match", {
+        encoding: "utf8",
+      }).trim();
+    } catch (error) {
+      console.log("> No match tag found for current commit.");
+    }
+
+    console.log("\n");
+    console.log("Commit message:", commitMessage);
+    console.log("Commit author:", commitAuthor);
+    console.log("Commit hash:", commitHash);
+    console.log("Branch name:", branchName);
+    console.log("Tag:", tagName);
+
+    // if (commitMessage.includes("Merge") && !tagName) {
+    //   console.log("> Merge commit, skipping");
+    //   return;
+    // }
+    if (commitMessage.includes("disable share")) {
+      console.log("> Disable share commit, skipping");
+      return;
+    }
+
+    // Extract dev-XXXX from branch name
+    const devId = branchName.match(/dev-\d+/i);
+    const clickupUrl = devId
+      ? `(<https://app.clickup.com/t/4663973/${devId[0]}|ClickUp>)`
+      : "";
+
+    const githubUrl = `(<https://github.com/Spectoda/spectoda-monorepo/commit/${commitHash}|GitHub>)`;
+
+    const studioLink = getStudioLinkForBranch(branchName)
+      ? `(<${getStudioLinkForBranch(branchName)}|Link>)`
+      : "";
+
+    if (tagName) {
+      // Tag specific message
+      message = `
+>>> *New tag by ${commitAuthor}*
+branch \`${branchName}\` ${githubUrl} ${clickupUrl} ${studioLink}
+
+Tag: \`${tagName}\`
+
+${commitMessage}`;
+    } else {
+      // Commit message
+      message = `
+>>> *New commit by ${commitAuthor}*
+branch \`${branchName}\` ${githubUrl} ${clickupUrl} ${studioLink}
+
+${commitMessage}`;
+    }
+
+    // Post a message to a channel
+    const response = await slackApi.post("/chat.postMessage", {
+      channel: TARGET_CHANNEL_ID,
+      text: message,
+      mrkdwn: true,
+    });
+
+    console.log(response.data);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+postCommitToSlack();
+
+function getStudioLinkForBranch(branch) {
+  if (branch === "0.9") {
+    return "https://0.9.studio.spectoda.com/";
+  } else if (branch === "0.9-dev") {
+    return "https://09dev.studio.spectoda.com/";
+  } else if (branch === "staging") {
+    return "https://staging-studio.up.railway.app/";
+  }
+}
