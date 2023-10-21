8116d42b11464ae9ff31f234a6fc4ea391eab9ef
Matěj Suchánek <jsem@matejsuchanek.cz>
2023-10-21 15:55:05 +0200

    chore: update to DEBUG_0.10.0_20231021 webassembly

--- src/Spectoda_JS.js
+++ src/Spectoda_JS.js
@@ -2,7 +2,7 @@ import { TextureLoader } from "three";
 import { logging } from "../logging";
 import { SpectodaWasm } from "./SpectodaWasm.js";
 
-const WASM_VERSION = "DEBUG_0.10.0_20231020";
+const WASM_VERSION = "DEBUG_0.10.0_20231021";
 
 export const COMMAND_FLAGS = Object.freeze({
   FLAG_UNSUPPORTED_COMMND_RESPONSE: 255, // TODO change FLAG_OTA_BEGIN to not be 255.
