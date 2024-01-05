63e50466908e3b672ca7c508be81328ff8d92fbf
Michal Kučera <michal.kucera04@gmail.com>
2024-01-05 13:45:18 +0100

    chore: cleanup

--- logging.ts
+++ logging.ts
@@ -17,11 +17,11 @@ export var logging = {
 export function setLoggingLevel(level: number) {
   logging.level = level;
   console.warn("SETTING DEBUG LEVEL TO:", logging.level);
-  logging.error = logging.level >= 1 ? console.error : function (...msg) { };
-  logging.warn = logging.level >= 2 ? console.warn : function (...msg) { };
-  logging.info = logging.level >= 3 ? console.log : function (...msg) { };
-  logging.debug = logging.level >= 4 ? console.log : function (...msg) { };
-  logging.verbose = logging.level >= 5 ? console.log : function (...msg) { };
+  logging.error = logging.level >= 1 ? console.error : function (...msg) {};
+  logging.warn = logging.level >= 2 ? console.warn : function (...msg) {};
+  logging.info = logging.level >= 3 ? console.log : function (...msg) {};
+  logging.debug = logging.level >= 4 ? console.log : function (...msg) {};
+  logging.verbose = logging.level >= 5 ? console.log : function (...msg) {};
 }
 
 export function routeLoggingElswhere(funcn: any) {
@@ -33,25 +33,10 @@ export function routeLoggingElswhere(funcn: any) {
 }
 
 if (typeof window !== "undefined") {
-  // window.DEBUG_LEVEL_NONE = DEBUG_LEVEL_NONE;
-  // window.DEBUG_LEVEL_ERROR = DEBUG_LEVEL_ERROR;
-  // window.DEBUG_LEVEL_WARN = DEBUG_LEVEL_WARN;
-  // window.DEBUG_LEVEL_INFO = DEBUG_LEVEL_INFO;
-  // window.DEBUG_LEVEL_DEBUG = DEBUG_LEVEL_DEBUG;
-  // window.DEBUG_LEVEL_VERBOSE = DEBUG_LEVEL_VERBOSE;
-
-  // @ts-ignore
   window.setLoggingLevel = setLoggingLevel;
 }
 
 if (globalThis) {
-  // globalThis.DEBUG_LEVEL_NONE = DEBUG_LEVEL_NONE;
-  // globalThis.DEBUG_LEVEL_ERROR = DEBUG_LEVEL_ERROR;
-  // globalThis.DEBUG_LEVEL_WARN = DEBUG_LEVEL_WARN;
-  // globalThis.DEBUG_LEVEL_INFO = DEBUG_LEVEL_INFO;
-  // globalThis.DEBUG_LEVEL_DEBUG = DEBUG_LEVEL_DEBUG;
-  // globalThis.DEBUG_LEVEL_VERBOSE = DEBUG_LEVEL_VERBOSE;
-
   // @ts-ignore
   globalThis.setLoggingLevel = setLoggingLevel;
 }
