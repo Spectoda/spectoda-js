69356d779ff12b949d6f67bffea228f1a605bdb3
Matěj Suchánek <jsem@matejsuchanek.cz>
2024-01-06 09:12:11 +0100

    refactor: wake-lock moved. Needs to be refactored for it to work

--- src/Spectoda.ts
+++ src/Spectoda.ts
@@ -250,65 +250,7 @@ export class Spectoda {
     return true;
   }
 
-  requestWakeLock(prioritized = false) {
-    logging.debug("> Activating wakeLock...");
 
-    if (prioritized) {
-      this.#isPrioritizedWakelock = true;
-    }
-
-    try {
-      if (detectNode()) {
-        // NOP
-      } else if (detectSpectodaConnect()) {
-        window.flutter_inappwebview.callHandler("setWakeLock", true);
-      } else {
-        navigator.wakeLock
-          .request("screen")
-          .then(Wakelock => {
-            logging.info("Web Wakelock activated.");
-            this.#wakeLock = Wakelock;
-          })
-          .catch(() => {
-            logging.warn("Web Wakelock activation failed.");
-          });
-      }
-      return Promise.resolve();
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  }
-
-  releaseWakeLock(prioritized = false) {
-    logging.debug("> Deactivating wakeLock...");
-
-    if (prioritized) {
-      this.#isPrioritizedWakelock = false;
-    } else if (this.#isPrioritizedWakelock) {
-      return Promise.resolve();
-    }
-
-    try {
-      if (detectNode()) {
-        // NOP
-      } else if (detectSpectodaConnect()) {
-        window.flutter_inappwebview.callHandler("setWakeLock", false);
-      } else {
-        this.#wakeLock
-          ?.release()
-          .then(() => {
-            logging.info("Web Wakelock deactivated.");
-            this.#wakeLock = null;
-          })
-          .catch(() => {
-            logging.warn("Web Wakelock deactivation failed.");
-          });
-      }
-      return Promise.resolve();
-    } catch (e) {
-      return Promise.reject(e);
-    }
-  }
 
   /**
    * @deprecated choose connector in the connect() function
