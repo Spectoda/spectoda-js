665ef96983556fa584bab88402fc4bd322aa3a7d
Matěj Suchánek <jsem@matejsuchanek.cz>
2024-05-24 16:10:26 +0200

    feat: spectoda.update() to git pull and npm install changes

--- Spectoda.js
+++ Spectoda.js
@@ -2356,4 +2356,31 @@ export class Spectoda {
       }
     }
   }
+
+  update() {
+    if (detectNode()) {
+      // run git pull and git submodule update
+      const { exec } = require("child_process");
+      exec("git pull && git submodule update --init --recursive", (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+
+      // run npm install
+      exec("npm install", (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+
+      process.exit(1);
+    }
+  }
 }
