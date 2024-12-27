4b03017f03ef865e0f361bbf527e00f0b0533460
Michal Kučera <70275735+mchlkucera@users.noreply.github.com>
2024-12-27 12:05:02 +0100

    feat: add __mocks__ folder (#21)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new mock data structure for scan results, enhancing
    testing capabilities.
    - Added the ability to import the new mock scan results from the main
    module.
    
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ export * from "./Spectoda";
 export * from "./deprecated_store/index";
 export * from "./src/types/spectoda-js-events";
 export type * from "./types";
+
+export { mockScanResult } from "./__mocks__/scan";
