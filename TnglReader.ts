e4cccc89bd0c0373f72cfedf334af67899b427f5
Matěj Suchánek <jsem@matejsuchanek.cz>
2025-01-17 08:37:06 +0100

    Revert "style: auto format code"
    This reverts commit f9932f9118ac1a2978a0f21870bc74a55368626a.

--- TnglReader.ts
+++ TnglReader.ts
@@ -1,210 +1,179 @@
-import { logging } from './logging'
+import { logging } from './logging';
 
 export class TnglReader {
-  #dataView: DataView
-  #index: number
+  #dataView: DataView;
+  #index: number;
 
   constructor(bytecode: Uint8Array) {
-    this.#dataView = new DataView(bytecode.buffer)
-    this.#index = 0
+    this.#dataView = new DataView(bytecode.buffer);
+    this.#index = 0;
   }
 
   // TODO optimize and test this function
   peekValue(byteCount: number, unsigned = true) {
     if (byteCount > 8) {
-      logging.error('peekValue(): ByteCountOutOfRange byteCount=', byteCount)
-      throw new RangeError('ByteCountOutOfRange')
+      logging.error('peekValue(): ByteCountOutOfRange byteCount=', byteCount);
+      throw new RangeError('ByteCountOutOfRange');
     }
 
     if (this.#index + byteCount > this.#dataView.byteLength) {
-      console.error(
-        'peekValue(): ReadOutOfRange index=',
-        this.#index,
-        'byteCount=',
-        byteCount,
-        'byteLength=',
-        this.#dataView.byteLength,
-      )
-      throw new RangeError('ReadOutOfRange')
+      console.error('peekValue(): ReadOutOfRange index=', this.#index, 'byteCount=', byteCount, 'byteLength=', this.#dataView.byteLength);
+      throw new RangeError('ReadOutOfRange');
     }
 
-    let value = 0n
+    let value = 0n;
 
     for (let i = byteCount; i > 0; i--) {
-      value <<= 8n
-      value |= BigInt(this.#dataView.getUint8(this.#index + i - 1))
+      value <<= 8n;
+      value |= BigInt(this.#dataView.getUint8(this.#index + i - 1));
     }
 
-    let result = value
+    let result = value;
 
     // Check if the sign bit is set
     if (!unsigned && value & (1n << (BigInt(byteCount * 8) - 1n))) {
       // Two's complement conversion
-      result = value - (1n << BigInt(byteCount * 8))
+      result = value - (1n << BigInt(byteCount * 8));
     }
 
-    if (
-      result > BigInt(Number.MAX_SAFE_INTEGER) ||
-      result < BigInt(Number.MIN_SAFE_INTEGER)
-    ) {
-      logging.error('peekValue(): Value is outside of safe integer range')
+    if (result > BigInt(Number.MAX_SAFE_INTEGER) || result < BigInt(Number.MIN_SAFE_INTEGER)) {
+      logging.error('peekValue(): Value is outside of safe integer range');
       // TODO handle this error better than loosing precision in conversion to Number
-      throw new RangeError('ValueOutOfRange')
+      throw new RangeError('ValueOutOfRange');
     }
 
-    return Number(result)
+    return Number(result);
   }
 
   readValue(byteCount: number, unsigned: boolean) {
     try {
-      const val = this.peekValue(byteCount, unsigned)
+      const val = this.peekValue(byteCount, unsigned);
 
-      this.forward(byteCount)
-      return val
+      this.forward(byteCount);
+      return val;
     } catch {
-      console.error(
-        'readValue(): ReadOutOfRange index=',
-        this.#index,
-        'byteCount=',
-        byteCount,
-        'byteLength=',
-        this.#dataView.byteLength,
-      )
-      throw 'ReadOutOfRange'
+      console.error('readValue(): ReadOutOfRange index=', this.#index, 'byteCount=', byteCount, 'byteLength=', this.#dataView.byteLength);
+      throw 'ReadOutOfRange';
     }
   }
 
   readBytes(byteCount: number) {
     if (this.#index + byteCount <= this.#dataView.byteLength) {
-      const bytes = []
+      const bytes = [];
 
       for (let i = 0; i < byteCount; i++) {
-        bytes.push(this.#dataView.getUint8(this.#index + i))
+        bytes.push(this.#dataView.getUint8(this.#index + i));
       }
 
-      this.forward(byteCount)
+      this.forward(byteCount);
 
-      return bytes
+      return bytes;
     } else {
-      console.error(
-        'readBytes(): ReadOutOfRange index=',
-        this.#index,
-        'byteCount=',
-        byteCount,
-        'byteLength=',
-        this.#dataView.byteLength,
-      )
-      throw 'ReadOutOfRange'
+      console.error('readBytes(): ReadOutOfRange index=', this.#index, 'byteCount=', byteCount, 'byteLength=', this.#dataView.byteLength);
+      throw 'ReadOutOfRange';
     }
   }
 
   readString(byteCount: number) {
     if (this.#index + byteCount <= this.#dataView.byteLength) {
-      let string = ''
-      let endOfTheString = false
+      let string = '';
+      let endOfTheString = false;
 
       for (let i = 0; i < byteCount; i++) {
-        const charCode = this.#dataView.getUint8(this.#index + i)
+        const charCode = this.#dataView.getUint8(this.#index + i);
 
         if (charCode === 0) {
-          endOfTheString = true
+          endOfTheString = true;
         }
         if (!endOfTheString) {
-          string += String.fromCharCode(charCode)
+          string += String.fromCharCode(charCode);
         }
       }
 
-      this.forward(byteCount)
+      this.forward(byteCount);
 
-      return string
+      return string;
     } else {
-      console.error(
-        'readString(): ReadOutOfRange index=',
-        this.#index,
-        'byteCount=',
-        byteCount,
-        'byteLength=',
-        this.#dataView.byteLength,
-      )
-      throw 'ReadOutOfRange'
+      console.error('readString(): ReadOutOfRange index=', this.#index, 'byteCount=', byteCount, 'byteLength=', this.#dataView.byteLength);
+      throw 'ReadOutOfRange';
     }
   }
 
   peekFlag() {
-    return this.peekValue(1, true)
+    return this.peekValue(1, true);
   }
 
   readFlag() {
-    return this.readValue(1, true)
+    return this.readValue(1, true);
   }
 
   readInt8() {
-    return this.readValue(1, false)
+    return this.readValue(1, false);
   }
 
   readUint8() {
-    return this.readValue(1, true)
+    return this.readValue(1, true);
   }
 
   readInt16() {
-    return this.readValue(2, false)
+    return this.readValue(2, false);
   }
 
   readUint16() {
-    return this.readValue(2, true)
+    return this.readValue(2, true);
   }
 
   readInt32() {
-    return this.readValue(4, false)
+    return this.readValue(4, false);
   }
 
   readUint32() {
-    return this.readValue(4, true)
+    return this.readValue(4, true);
   }
 
   readInt48() {
-    return this.readValue(6, false)
+    return this.readValue(6, false);
   }
 
   readUint48() {
-    return this.readValue(6, true)
+    return this.readValue(6, true);
   }
 
   readInt64() {
-    return this.readValue(8, false)
+    return this.readValue(8, false);
   }
 
   readUint64() {
-    return this.readValue(8, true)
+    return this.readValue(8, true);
   }
 
   get available() {
-    return this.#dataView.byteLength - this.#index
+    return this.#dataView.byteLength - this.#index;
   }
 
   moveForward(byteCount: number) {
     if (this.#index + byteCount <= this.#dataView.byteLength) {
-      this.#index += byteCount
+      this.#index += byteCount;
     } else {
-      this.#index = this.#dataView.byteLength
+      this.#index = this.#dataView.byteLength;
     }
   }
 
   /* @deprecated use moveForward instead */
   forward(byteCount: number) {
-    this.moveForward(byteCount)
+    this.moveForward(byteCount);
   }
 
   moveBack(byteCount: number) {
     if (this.#index >= byteCount) {
-      this.#index -= byteCount
+      this.#index -= byteCount;
     } else {
-      this.#index = 0
+      this.#index = 0;
     }
   }
 
   /* @deprecated use moveBack instead */
   back(byteCount: number) {
-    this.moveBack(byteCount)
+    this.moveBack(byteCount);
   }
 }
--- logging.ts
+++ logging.ts
@@ -1,21 +1,21 @@
 /**
  * @deprecated use logging.LOGGING_LEVEL_NONE */
-export const DEBUG_LEVEL_NONE = 0
+export const DEBUG_LEVEL_NONE = 0;
 /**
  * @deprecated use logging.LOGGING_LEVEL_ERROR */
-export const DEBUG_LEVEL_ERROR = 1
+export const DEBUG_LEVEL_ERROR = 1;
 /**
  * @deprecated use logging.LOGGING_LEVEL_WARN */
-export const DEBUG_LEVEL_WARN = 2
+export const DEBUG_LEVEL_WARN = 2;
 /**
  * @deprecated use logging.LOGGING_LEVEL_INFO */
-export const DEBUG_LEVEL_INFO = 3
+export const DEBUG_LEVEL_INFO = 3;
 /**
  * @deprecated use logging.LOGGING_LEVEL_DEBUG */
-export const DEBUG_LEVEL_DEBUG = 4
+export const DEBUG_LEVEL_DEBUG = 4;
 /**
  * @deprecated use logging.LOGGING_LEVEL_VERBOSE */
-export const DEBUG_LEVEL_VERBOSE = 5
+export const DEBUG_LEVEL_VERBOSE = 5;
 
 // Logging configuration object
 export const logging = {
@@ -34,42 +34,42 @@ export const logging = {
 
   setLoggingLevel: (level: number) => {
     if (level >= 0 && level <= 5) {
-      logging.level = level
+      logging.level = level;
     }
-    logging.log = logging.level >= 0 ? logging.logCallback : () => {}
-    logging.error = logging.level >= 1 ? logging.errorCallback : () => {}
-    logging.warn = logging.level >= 2 ? logging.warnCallback : () => {}
-    logging.info = logging.level >= 3 ? logging.logCallback : () => {}
-    logging.debug = logging.level >= 4 ? logging.logCallback : () => {}
-    logging.verbose = logging.level >= 5 ? logging.logCallback : () => {}
+    logging.log = logging.level >= 0 ? logging.logCallback : () => {};
+    logging.error = logging.level >= 1 ? logging.errorCallback : () => {};
+    logging.warn = logging.level >= 2 ? logging.warnCallback : () => {};
+    logging.info = logging.level >= 3 ? logging.logCallback : () => {};
+    logging.debug = logging.level >= 4 ? logging.logCallback : () => {};
+    logging.verbose = logging.level >= 5 ? logging.logCallback : () => {};
   },
 
   /**
    * @deprecated use setLoggingLevel
    */
   setDebugLevel: (level: number) => {
-    logging.setLoggingLevel(level)
+    logging.setLoggingLevel(level);
   },
 
   setLogCallback(callback: (...msg: any) => void) {
-    logging.logCallback = callback
+    logging.logCallback = callback;
     //? reassing log function to the new callback
-    logging.log = logging.level >= 0 ? logging.logCallback : () => {}
-    logging.info = logging.level >= 3 ? logging.logCallback : () => {}
-    logging.debug = logging.level >= 4 ? logging.logCallback : () => {}
-    logging.verbose = logging.level >= 5 ? logging.logCallback : () => {}
+    logging.log = logging.level >= 0 ? logging.logCallback : () => {};
+    logging.info = logging.level >= 3 ? logging.logCallback : () => {};
+    logging.debug = logging.level >= 4 ? logging.logCallback : () => {};
+    logging.verbose = logging.level >= 5 ? logging.logCallback : () => {};
   },
 
   setWarnCallback(callback: (...msg: any) => void) {
-    logging.warnCallback = callback
+    logging.warnCallback = callback;
     //? reassing warn function to the new callback
-    logging.warn = logging.level >= 2 ? logging.warnCallback : () => {}
+    logging.warn = logging.level >= 2 ? logging.warnCallback : () => {};
   },
 
   setErrorCallback(callback: (...msg: any) => void) {
-    logging.errorCallback = callback
+    logging.errorCallback = callback;
     //? reassing error function to the new callback
-    logging.error = logging.level >= 1 ? logging.errorCallback : () => {}
+    logging.error = logging.level >= 1 ? logging.errorCallback : () => {};
   },
 
   log: (...msg: any) => logging.logCallback(...msg),
@@ -78,10 +78,10 @@ export const logging = {
   info: (...msg: any) => logging.logCallback(...msg),
   debug: (...msg: any) => logging.logCallback(...msg),
   verbose: (...msg: any) => logging.logCallback(...msg),
-}
+};
 
-logging.setLoggingLevel(3)
+logging.setLoggingLevel(3);
 
 if (typeof window !== 'undefined') {
-  window.logging = logging
+  window.logging = logging;
 }
