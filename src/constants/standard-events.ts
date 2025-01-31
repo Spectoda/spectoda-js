873f954116b1387aa1be16c6760160bc8de56b3e
Michal Kučera <michal.kucera04@gmail.com>
2025-01-31 09:53:46 +0100

    docs: deprecated STANDARD_EVENT_LABELS

--- src/constants/standard-events.ts
+++ src/constants/standard-events.ts
@@ -1,7 +1,11 @@
+/**
+ * @deprecated
+ * Use {@link [SPECTODA_EVENT_LABELS] packages/spectoda-core/src/js/utils/constants.ts} instead.
+ */
 export const STANDARD_EVENT_LABELS = Object.freeze({
   BRIGHTNESS: 'brigh',
   TOGGLE_ON_OFF: 'toggl',
   COLOR: 'color',
   TEMPERATURE: 'tempe',
   ANIMATION: 'anima',
-});
+})
