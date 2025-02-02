472a66d69151f01d42eadc153186a7939c1f99c8
Martin Weiser <martin.weiser@gmail.com>
2025-02-02 21:21:59 +0100

    Chore/comments (#43)

--- src/types/messages.ts
+++ src/types/messages.ts
@@ -17,17 +17,3 @@ export type ControllerWarning = {
   warnings: SpectodaWarning[]
 }
 
-// TODO remove this comment once implemented in spectoda-core
-/** On spectoda-core side:
-* const SPECTODA_MESSAGE_TYPE = Object.freeze({
-  ERROR: 'error',
-  WARNING: 'warning',
-})
- * export type SpectodaMessage = {
-  description: string
-  code: number
-  possibleSolutionAction: Promise<void>;
-  possibleSolution: string;
-  type: keyof typeof SPECTODA_MESSAGE_TYPE;
-}
- */
