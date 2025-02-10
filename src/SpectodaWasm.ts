0c507aa584d2b3f4643d02d91a746e1499050ba4
Matěj Suchánek <jsem@matejsuchanek.cz>
2025-02-10 08:15:38 +0100

    chore: update WASM

--- src/SpectodaWasm.ts
+++ src/SpectodaWasm.ts
@@ -4,7 +4,7 @@ import { logging } from '../logging'
 
 import { MainModule, Uint8Vector } from './types/wasm'
 
-const WASM_VERSION = 'DEBUG_DEV_0.12.5_20250209'
+const WASM_VERSION = 'DEBUG_DEV_0.12.5_20250210'
 
 let moduleInitilizing = false
 let moduleInitilized = false
