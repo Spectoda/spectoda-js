090b7de9854df8e0e2259190281fa8a71ed42bc4
Michal Kučera <michal.kucera04@gmail.com>
2025-04-04 08:01:32 +0200

    fix: export constants

--- v012/index.ts
+++ v012/index.ts
@@ -1,6 +1,7 @@
-export * from './Spectoda'
 export * from './functions'
 export * from './logging'
+export * from './Spectoda'
+export * from './src/constants'
 export * from './src/types/app-events'
 export type * from './types'
 
