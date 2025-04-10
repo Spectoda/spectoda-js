3ff4b20257419142b719dccc431dcbe6f248acf8
Michal Kučera <michal.kucera04@gmail.com>
2025-04-10 10:16:22 +0200

    refactor: explicitly export types

--- v012/index.ts
+++ v012/index.ts
@@ -2,6 +2,40 @@ export * from './Spectoda'
 export * from './functions'
 export * from './logging'
 export * from './src/types/app-events'
-export type * from './types'
+
+export type { SpectodaTypes } from './src/types/primitives'
+export type {
+  SpectodaEvent,
+  SpectodaNumberEvent,
+  SpectodaLabelEvent,
+  SpectodaPercentageEvent,
+  SpectodaTimestampEvent,
+  SpectodaColorEvent,
+  SpectodaPixelsEvent,
+  SpectodaBooleanEvent,
+  SpectodaNullEvent,
+  SpectodaUndefinedEvent,
+} from './src/types/event'
+
+export { EventSchema } from './src/schemas/event'
+export {
+  FirmwareVersionSchema,
+  FirmwareVersionFullSchema,
+  ProductCodeSchema,
+  NetworkSignatureSchema,
+  MacAddressSchema,
+  BaudrateSchema,
+  ControllerNameSchema,
+  PathSchema,
+  TimestampSchema,
+  PercentageSchema,
+  LabelSchema,
+  NumberSchema,
+  ColorSchema,
+  PixelsSchema,
+  BooleanSchema,
+  NullSchema,
+  UndefinedSchema,
+} from './src/schemas/primitives'
 
 export { mockScanResult } from './__mocks__/scan'
