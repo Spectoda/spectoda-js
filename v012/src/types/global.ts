e9b2ad1a92a661159f4404f1a2b90b0e9fac867d
Michal Kučera <michal.kucera04@gmail.com>
2025-04-10 10:16:09 +0200

    chore: clenaup unused window.stuff

---
+++ v012/src/types/global.ts
@@ -0,0 +1,13 @@
+import { SCFBridgeMethods } from '@spectoda/scf-bridge/src/types'
+
+import { validateTimestamp } from '../../functions'
+import { logging } from '../../logging'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Window {
+    logging: typeof logging
+    validateTimestamp: typeof validateTimestamp
+    flutter_inappwebview: SCFBridgeMethods
+  }
+}
