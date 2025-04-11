3ec94531058524fe3d4b58f2075d1dcda977c422
Yamiteru <yamiteru@icloud.com>
2025-04-11 10:49:15 +0200

    feat: add exports (#73)

--- v012/index.ts
+++ v012/index.ts
@@ -15,7 +15,20 @@ export type {
   SpectodaBooleanEvent,
   SpectodaNullEvent,
   SpectodaUndefinedEvent,
+  SpectodaEventStateValue,
 } from './src/types/event'
+
+export { CONNECTORS, BROADCAST_ID, NO_NETWORK_SIGNATURE, JS_VALUE_LIMITS } from './src/constants'
+
+export { VALUE_TYPES } from './src/constants/values'
+
+export { CONNECTION_STATUS, ConnectorType, ConnectionStatus } from './src/types/connect'
+
+export { ControllerError, ControllerWarning, SpectodaMessage } from './src/types/messages'
+
+export { deactivateDebugMode, enableDebugMode, uint8ArrayToHexString } from './functions'
+
+export { isCurrentSpectodaInstanceLocal, createSpectodaWebsocket } from './SpectodaWebSocketsConnector'
 
 export { EventSchema, EventInputSchema } from './src/schemas/event'
 export {
